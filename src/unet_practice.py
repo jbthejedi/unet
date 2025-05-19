3728876ee7355b0f738d6386e80f11494e456e10
Justin Barry <justinbarrythejedi@gmail.com>
2025-05-19 10:06:42 +0700

    ignoring files & updated code

--- src/unet_practice.py
+++ src/unet_practice.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from tqdm import tqdm
 from dataclasses import dataclass
 from torchvision.datasets import OxfordIIITPet
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, Subset, random_split
 
 
 seed = 1337
@@ -17,12 +17,19 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 @dataclass
 class Config:
-    name : str = "unet-oxford-pet"
-    run : str = "unet-run"
+    project : str = "unet-oxford-pet"
+    name : str = "unet-run"
     lr : float = 1e-3
     image_size : int = 128
-    batch_size : int = 4
-    n_epochs : int = 2
+    batch_size : int = 16
+    n_epochs : int = 10
+
+    sample_size : int = 100
+    test_run : bool = True
+    wandb_active : str = 'disabled'
+
+    # test_run : bool = False
+    #wandb_active : str = 'online'
 
 
 class DoubleConv(nn.Module):
@@ -85,12 +92,31 @@ class UNet(nn.Module):
 
         return self.out(u1)
 
+def dice_score(preds, labels, num_classes=3, eps=1e-6):
+    """Computes mean Dice score for a batch."""
+    print(f"preds.shape {preds.shape}")
+    preds = torch.argmax(preds, dim=1)  # [B, H, W]
+    dice = 0.0
+    for c in range(num_classes):
+        pred_c = (preds == c).float()
+        label_c = (labels == c).float()
+        intersection = (pred_c * label_c).sum()
+        union = pred_c.sum() + label_c.sum()
+        dice += (2. * intersection + eps) / (union + eps)
+    return dice / num_classes
+
 def train_and_validate_model(config : Config):
     wandb.init(
-        mode='disabled'
+        project=config.project,
+        name=config.name,
+        config=vars(config),
+        mode=config.wandb_active
     )
     transform = T.Compose([
         T.Resize((config.image_size, config.image_size)),
+        # T.RandomHorizontalFlip(),
+        # T.ColorJitter(),
+        # T.RandomResizedCrop(config.image_size, scale=(0.8, 1.0)),
         T.ToTensor(),
     ])
     target_transform = T.Compose([
@@ -100,12 +126,17 @@ def train_and_validate_model(config : Config):
     ])
 
     dataset = OxfordIIITPet(
-        root='.',
-        download=True,
+        root='/Users/justinbarry/projects/unet.bk/src/data/',
+        download=False,
         target_types='segmentation',
         transform=transform,
         target_transform=target_transform
     )
+    
+    import random
+    if config.test_run:
+        indices = random.sample(range(len(dataset)), config.sample_size)
+        dataset = Subset(dataset, indices)
 
     train_len = int(0.9 * len(dataset))
     train_ds, val_ds = random_split(dataset, [train_len, len(dataset) - train_len])
@@ -141,24 +172,49 @@ def train_and_validate_model(config : Config):
                 loss.backward()
                 optimizer.step()
 
-                train_loss = loss.item() * xb.size(0)
                 _, preds = torch.max(logits, 1)
+
+                train_loss += loss.item() * yb.numel()
                 train_correct += (preds == yb).sum().item()
-                train_total += xb.size(0)
-            train_epoch_loss += train_loss / train_total
-            train_epoch_acc += train_correct / train_total
-            pbar.set_postfix(f"Train Loss {train_epoch_loss} Train Acc {train_epoch_acc}")
+                train_total += yb.numel()
+                pbar.set_postfix(loss=loss.item())
+
+            train_epoch_loss = train_loss / train_total
+            train_epoch_acc = train_correct / train_total
+            tqdm.write(f"Train Loss {train_epoch_loss:.4f} Train Acc {train_epoch_acc:.2f}")
 
         model.eval()
         with tqdm(val_dl, desc="Validation") as pbar:
             with torch.no_grad():
+                val_loss = 0
+                val_correct = 0.0
+                val_total = 0
                 for xb, yb in pbar:
                     xb = xb.to(device)
                     yb = yb.squeeze(1).long().to(device)
                     logits = model(xb)
-                    total_loss += criterion(preds, yb).item()
-            pbar.set_postfix(f"Val Loss {total_loss}")
+                    loss = criterion(logits, yb)
 
+                    _, preds = torch.max(logits, 1)
+                    val_correct += (preds == yb).sum().item()
+                    val_loss += loss.item() * yb.numel()
+                    val_total += yb.numel()
+                    pbar.set_postfix(loss=loss.item())
+
+                val_epoch_loss = val_loss / val_total
+                val_epoch_acc = val_correct / val_total
+                tqdm.write(f"Val Loss {val_epoch_loss:.4f} Val Acc {val_epoch_acc:.2f}")
+                pbar.set_postfix(loss=loss.item())
+
+        wandb.log({
+            "epoch": epoch,
+            "train/loss": train_epoch_loss,
+            "train/acc": train_epoch_acc,
+            "val/loss": val_epoch_loss,
+            "val/acc": val_epoch_acc,
+            "lr": optimizer.param_groups[0]['lr']
+        })
+    wandb.finish()
 
 
 def main():
